aad0e7859c15e5b3002adfa73835345037822be0
shiva2502 <shivavisit28@gmail.com>
2026-09-03 07:40:38 +0200

    variables added

---
+++ 01_Basics/1_variables.js
@@ -0,0 +1,14 @@
+const username = "shiva2502";
+let email = "srinija@js.com"
+let password = 'shresta'
+let accountname;
+
+console.log(username);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([username,email,password,accountname])
+

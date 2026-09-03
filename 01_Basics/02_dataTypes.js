58b424ed9ddc9153f43c81d94ab36798361a085d
shiva2502 <shivavisit28@gmail.com>
2026-09-03 07:39:12 +0200

    datatypes added

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+console.log(3 +
+    3)  // code readability should be high
+console.log("Shiva")
+
+
+// JS official documentation https://tc39.es/
+
+// JS documentation refer https://developer.mozilla.org/en-US/docs/Web/JavaScript
+ 
+
+let name = "Shiva"  // string
+let age = 18 // int
+let isLoggedIn = false // boolean
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof "shiva"); //string
+console.log(typeof 33); //number
+console.log(typeof undefined); //undefined
+console.log(typeof null); //null
+
+
+

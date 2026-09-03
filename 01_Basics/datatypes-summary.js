e88801e1bf8979566ce713690f124fa558002d18
shiva2502 <shivavisit28@gmail.com>
2026-09-03 14:29:58 +0200

    dataypes summary Added

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,49 @@
+// Datatypes categorized in two types
+
+/*
+Primitive datatypes
+====================
+7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+*/
+const score = 100;
+console.log(typeof score); // number
+
+const scoreValue = 100.3;
+console.log(typeof scoreValue); // number
+
+const isLoggedIn = false
+console.log(typeof isLoggedIn); // boolean
+
+const outsideTemp = null;
+console.log(typeof outsideTemp); // object
+
+let userEmail;
+console.log(typeof userEmail); // undefined
+
+const id =Symbol("123");
+console.log(typeof id);
+console.log(id); //Symbol(123)
+
+const anotherid =Symbol("123");
+console.log(anotherid); //Symbol(123)
+
+console.log(id === anotherid); // false
+
+const bigNumber = 963875421n
+console.log(typeof bigNumber);
+
+
+/*
+Reference datatypes (Non-Primitive datatypes)
+==============================================
+Array, Objects, Functions
+*/
+const heros = ["Shaktiman", "Spiderman", "Superman"];
+console.log(heros);
+
+
+let myObj = {
+    name : "shiva", age : 33
+}
+console.log(myObj);

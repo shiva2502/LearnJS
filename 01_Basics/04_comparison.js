782030f4bb61baaf999d1bca3b1634daeb07836b
shiva2502 <shivavisit28@gmail.com>
2026-09-03 13:53:49 +0200

    Comparison Added

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1); // true
+// console.log(2 >= 1); //true
+// console.log(2 < 1); // false
+// console.log(2 == 1); // false
+// console.log(2 != 1); //true
+
+/* Avoid the below kind of comparison always maintain a clean code */
+
+// console.log("2" > 1); // true
+// console.log("02" > 1); // true
+
+// console.log(null > 0); // false
+// console.log(null == 0); //false
+// console.log(null >= 0); //true
+
+// console.log(undefined == 0); // false
+// console.log(undefined > 0); // false
+// console.log(undefined < 0); // false
+
+ 

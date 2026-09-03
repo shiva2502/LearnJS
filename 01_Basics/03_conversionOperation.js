f61283e6143a1f8d1dda8a1453b28e290f394ad9
shiva2502 <shivavisit28@gmail.com>
2026-09-03 10:13:12 +0200

    Operations Added

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -15,12 +15,62 @@ let valueInNumber = Number(score);
 console.log(typeof valueInNumber); 
 console.log(valueInNumber);
 */
-//let isLoggedIn = 1 // true
-//let isLoggedIn = 0 // false
-//let isLoggedIn = "Shiva" // true
-//let isLoggedIn = " " //true
-let isLoggedIn = "" //false
+//let isLoggedIn = 1;      // true
+//let isLoggedIn = 0;     // false
+//let isLoggedIn = "Shiva"; // true
+//let isLoggedIn = " ";  //true
+let isLoggedIn = "";     //false
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
 
+// ************************************ Operations *********************************************
+
+let value =3
+let negvalue = -value
+
+// console.log(negvalue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello "
+let str2 = "Shiva"
+
+let str3 = str1+str2
+console.log(str3);
+
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); //122 // starts with string then string will be output
+// console.log(1 + 2 + "2"); //32
+
+console.log(+true); // 1
+console.log(+""); // 0
+
+let gameCounter = 100;
+//gameCounter++; // 101
+++gameCounter; //101
+console.log(gameCounter);
+
+/*
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+// The ++ operator is overloaded for two types of operands: number and BigInt. It first coerces the operand to a numeric value and tests the type of it. It performs BigInt increment if the operand becomes a BigInt; otherwise, it performs number increment.
+
+*/

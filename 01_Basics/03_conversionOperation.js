566d45cb18af99df44d443a116ef5f23aa4961aa
shiva2502 <shivavisit28@gmail.com>
2026-09-03 08:51:17 +0200

    conOperations added

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+// let score = "33abc" //NaN stands for "Not-a-Number"
+
+// let score = "33"; //number
+
+// let score = null; // 0
+
+// let score = undefined; //NaN
+
+/*
+let score = true; // boolean   (true => 1, false => 0)
+
+console.log(typeof (score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber); 
+console.log(valueInNumber);
+*/
+//let isLoggedIn = 1 // true
+//let isLoggedIn = 0 // false
+//let isLoggedIn = "Shiva" // true
+//let isLoggedIn = " " //true
+let isLoggedIn = "" //false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+

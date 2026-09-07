ea9a57ed75adb3764a208e4b15cdcbce8d678d0a
shiva2502 <shivavisit28@gmail.com>
2026-09-07 19:19:40 +0200

    Nums added

---
+++ 01_Basics/06_nums-and-maths.js
@@ -0,0 +1,19 @@
+const score = 600;
+console.log(score);
+
+const balance = new Number(963);
+console.log(balance);
+
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const anotherNumber = 123.456789
+console.log(anotherNumber.toPrecision(6));
+
+const hundreds = 1000000;
+console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString("en-IN")); // Refer MDN docs for different number counts as per country
+
+//****************************************Maths************************************************* */
+

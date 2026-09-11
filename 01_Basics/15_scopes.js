d93e9648c0b61ba9d17591b6dec55b44013bacf6
shiva2502 <shivavisit28@gmail.com>
2026-09-11 10:35:26 +0200

    Scopes Added

---
+++ 01_Basics/15_scopes.js
@@ -0,0 +1,54 @@
+// Scopes function inside function
+
+// function one(){
+//     const username = "Shiva"
+
+//     function two(){
+
+//         const website = "Youtube"
+//         console.log(username);
+        
+//     }
+
+//     // console.log(website);
+
+//     two()
+    
+// }
+
+// one()
+
+
+// if (true){
+
+//     const username = "Hitesh"
+//     if (username === "Hitesh"){
+//         const website = " Youtube"
+//         // console.log(username + website);
+        
+//     }
+
+//     //console.log(website);
+    
+// }
+
+// console.log(username);
+
+//************************************Interesting********************************//
+
+console.log(addOne(6))
+
+function addOne(num){
+
+    return num+1;
+}
+
+ addTwo(9)
+
+
+
+const addTwo = function(num){
+
+    return num+2
+}
+

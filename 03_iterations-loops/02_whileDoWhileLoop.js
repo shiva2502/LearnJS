ed1d48a318ba94665b2de35046f4a5b504d81f6f
shiva2502 <shivavisit28@gmail.com>
2026-09-12 14:04:53 +0200

    while & do while concept added

---
+++ 03_iterations-loops/02_whileDoWhileLoop.js
@@ -0,0 +1,33 @@
+ //while loop
+
+//  let index = 0;
+
+//  while(index <= 9){
+
+//     console.log(`value of index is ${index}`);
+//     index = index + 3;
+    
+//  }
+
+
+// let myArray = ["IronMan","Spiderman","Superman"]
+
+// let array = 0
+// while(array<myArray.length)
+// {
+//     console.log(`value is ${myArray[array]}`);
+//     array++;
+// }
+
+
+
+//*********do while***************/
+
+let score = 3;
+
+do{
+    console.log(`Score is ${score}`);
+    score++;
+    
+}
+while(score<=9);

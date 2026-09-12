fbecf9d79a42901713051d7ab451981e478c3337
shiva2502 <shivavisit28@gmail.com>
2026-09-12 15:16:39 +0200

    for of loop added

---
+++ 03_iterations-loops/03_array_loop.js
@@ -0,0 +1,68 @@
+// for of
+
+//["","",""]
+//[{},{},{}]
+
+// const array = [1,2,3,4,5,6]
+
+// for (const num of array) {
+
+//     console.log(`array of elements ${num}`);
+   
+            
+// }
+
+
+// const name = "Hello Shiva"
+
+// for (const alpha of name) {
+
+//     console.log(`Each char is ${alpha}`);
+    
+    
+// }
+
+
+
+//*************************   map  *****************************/
+//Duplicate values are not allowed in map
+const map = new Map()
+
+map.set("RS", "Russia");
+map.set("IN", "India");
+map.set("CN", "CHINA");
+map.set("IN", "INDIA") // Duplicate value no allowed
+
+//console.log(map);  //Map(3) { 'RS' => 'Russia', 'IN' => 'India', 'CN' => 'CHINA' }
+
+for (const keys of map) {
+    //console.log(keys);
+    
+}
+
+// [ 'RS', 'Russia' ]
+// [ 'IN', 'INDIA' ]
+// [ 'CN', 'CHINA' ]
+
+
+for (const [keys, value] of map) {
+    //console.log(keys,'-',value);
+    
+}
+
+// RS - Russia
+// IN - INDIA
+// CN - CHINA
+
+// Object isnot iteratable at forof loop
+
+const myobject ={
+    game1: 'badminton',
+    game2: 'peddal'
+}
+
+for (const [key, value] of myobject) {
+    console.log(key,'-',value);  //  myobject is not iterable
+    
+    
+}

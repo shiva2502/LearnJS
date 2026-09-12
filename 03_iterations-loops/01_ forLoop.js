b41b66d0fd00362ce33a0d8a5aa3d5a27c5d92f5
shiva2502 <shivavisit28@gmail.com>
2026-09-12 13:35:41 +0200

    for loop Added

---
+++ 03_iterations-loops/01_ forLoop.js
@@ -0,0 +1,70 @@
+//for-loop
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    if(element==6){
+        //console.log("6 is the best bumber");
+        
+    }
+    //console.log(element);
+    
+    
+}
+
+// console.log(element);
+
+
+
+// for (let i = 0; i <=10; i++) 
+// {
+//     console.log(`outer loop value: ${i}`);
+//     for (let j = 0; j <=10; j++) 
+//         {
+//         console.log(`inner loop value: ${j} and inner loop ${i}`);   
+//     }
+    
+// }
+
+
+// Array in loop
+
+// let myArray = ["IronMan","Spiderman","Superman"]
+// console.log(myArray.length);
+
+
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+    
+    
+// }
+
+
+//   break
+
+// for (let index = 0; index < 20; index++) {
+    
+//     if(index == 6){
+//         console.log(`Detected 6`);
+//         break;
+        
+//     }
+//     console.log(`value of i is ${index}`);
+    
+    
+// }
+
+
+// contiue
+
+for (let index = 0; index < 20; index++) {
+    
+    if(index == 6){
+        console.log(`Detected 6`);
+        continue;
+        
+    }
+    console.log(`value of i is ${index}`);
+    
+    
+}
